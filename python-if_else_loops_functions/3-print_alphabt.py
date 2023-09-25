e9d5b80983eebf52b235cf6d14f9f59af86609bc
ER1-53 <cremey.erwan@gmail.com>
2023-09-25 21:06:51 +0200

    creat print_comb2 & printhexa correction to print_alpha

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 for ascii in range(97, 123):
-    if chr(ascii) == 'q' or chr(ascii) == 'e':
-        continue
-    else:
+    if chr(ascii) != 'q' or chr(ascii) != 'e':
         print(chr(ascii), end='')
