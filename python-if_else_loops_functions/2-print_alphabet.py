fa046a99a1bfccbd9258bcd9a50c6efb02a8f122
ER1-53 <cremey.erwan@gmail.com>
2023-09-26 09:25:41 +0200

    change format of code with .format

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for ascii in range(97, 123):
-    print(chr(ascii), end='')
+    print("{}".format(chr(ascii)), end='')
--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 for ascii in range(97, 123):
-    if chr(ascii) != 'q' or chr(ascii) != 'e':
-        print(chr(ascii), end='')
+    if ascii == 101 or ascii == 113:
+        continue
+    else:
+        print("{}".format(chr(ascii)), end='')
