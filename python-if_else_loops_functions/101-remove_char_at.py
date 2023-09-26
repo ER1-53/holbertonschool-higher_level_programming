fd040f18cc12874c308b9157ac1b9e0cd5175dd8
ER1-53 <cremey.erwan@gmail.com>
2023-09-26 20:21:57 +0200

    added task 101.remove char at

--- python-if_else_loops_functions/101-remove_char_at.py
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+def remove_char_at(str, n):
+    for letter in range(len(str)):
+        if n < 0 or n > len(str):
+            return (str)
+        else:
+            return(str[0:n] + str[n + 1:])
+          
