4c31383fbcf0cf01b4fba0ba1127a47c17373327
ER1-53 <cremey.erwan@gmail.com>
2023-09-26 20:23:15 +0200

    pycodestyle correction

--- python-if_else_loops_functions/101-remove_char_at.py
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -4,5 +4,4 @@ def remove_char_at(str, n):
         if n < 0 or n > len(str):
             return (str)
         else:
-            return(str[0:n] + str[n + 1:])
-          
+            return (str[0:n] + str[n + 1:])
