a9278b5ee7419fcf9c165f65bbe610d7848f0cd1
ER1-53 <cremey.erwan@gmail.com>
2023-09-26 15:09:02 +0200

    8-upper style changing

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,13 +1,10 @@
 def uppercase(str):
     upp = ""
     for i in range(len(str)):
-        if ord('a')<= ord(str[i]) <= ord('z'):
+        if ord('a') <= ord(str[i]) <= ord('z'):
             maj = ord(str[i]) - 32
             upp = chr(maj)
         else:
             upp = str[i]
         print("{}".format(upp), end='')
     print()
-        
-            
-
