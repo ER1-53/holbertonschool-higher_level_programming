7d9e4fc57acdae05f0c8a88a6c7b460ec4505ece
ER1-53 <cremey.erwan@gmail.com>
2023-09-26 16:17:22 +0200

    creat advance task and task 100 do

---
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for ascii in reversed(range(97, 123)):
+    if ascii % 2 == 0:
+        upp = ascii 
+    else:
+        upp = ascii - 32
+    print("{}".format(chr(upp)), end='')
