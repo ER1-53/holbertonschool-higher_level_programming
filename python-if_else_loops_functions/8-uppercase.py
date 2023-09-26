b5b04392c62694d485b28f178cb2fec4d4957219
ER1-53 <cremey.erwan@gmail.com>
2023-09-26 15:05:27 +0200

    creat 8-uppercase

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,13 @@
+def uppercase(str):
+    upp = ""
+    for i in range(len(str)):
+        if ord('a')<= ord(str[i]) <= ord('z'):
+            maj = ord(str[i]) - 32
+            upp = chr(maj)
+        else:
+            upp = str[i]
+        print("{}".format(upp), end='')
+    print()
+        
+            
+
