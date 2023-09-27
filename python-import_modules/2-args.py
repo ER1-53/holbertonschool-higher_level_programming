bc48a8453b7d7375cea1822269c686fba07c90fd
ER1-53 <cremey.erwan@gmail.com>
2023-09-27 14:02:16 +0200

    creat 2-args

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+import sys
+argv = sys.argv
+argc = len(argv)
+print("{} arguments".format(argc), end="")
+if len(argv) != 1:
+    for i in range(1,argc):
+        print(":")
+        print("{}: {}".format(i, argv[i]), end='')
+        print()
+else:
+    print(".")
+        
