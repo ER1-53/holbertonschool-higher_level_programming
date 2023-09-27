9a8351558750d75a8218b4681f694a50301c5dbc
ER1-53 <cremey.erwan@gmail.com>
2023-09-27 14:10:33 +0200

    update L4 (delete print arg)  & L6 (creat print arg)  & L11 (creat print arg)

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,12 +2,11 @@
 import sys
 argv = sys.argv
 argc = len(argv)
-print("{} arguments".format(argc), end="")
 if len(argv) != 1:
+    print("{} arguments:".format(argc))
     for i in range(1,argc):
-        print(":")
         print("{}: {}".format(i, argv[i]), end='')
         print()
 else:
-    print(".")
+    print("{} argument.".format(argc))
         
