9b8ac7fc96766aee200ef23034b2a181e8ffe20f
ER1-53 <cremey.erwan@gmail.com>
2023-09-27 16:16:50 +0200

    simple code

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 import sys
 if __name__ == "__main__":
-    argv = sys.argv
-    argc = len(argv)
     result = 0
-    for i in range(1, argc):
-        result += int(argv[i])
+    for i in range(1, len(sys.argv)):
+        result += int(sys.argv[i])
     print("{}".format(result))
