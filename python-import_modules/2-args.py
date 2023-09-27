9b095b375397f9289c51236978caf84f4275ecd0
ER1-53 <cremey.erwan@gmail.com>
2023-09-27 14:28:23 +0200

    update with if name

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 import sys
-argv = sys.argv
-argc = len(argv)
-if (argc - 1) == 1:
-    argu = "argument"
-else:
-    argu = "arguments"
-print("{} {}".format(argc - 1, argu), end="")
-if len(argv) != 1:
-    print(":")
-    for i in range(1,argc):
-        print("{}: {}".format(i, argv[i]), end='')
-        print()
-else:
-    print(".")
+if __name__ == "__main__":
+    argv = sys.argv
+    argc = len(argv)
+    if (argc - 1) == 1:
+        argu = "argument"
+    else:
+        argu = "arguments"
+    print("{} {}".format(argc - 1, argu), end="")
+    if len(argv) != 1:
+        print(":")
+        for i in range(1,argc):
+            print("{}: {}".format(i, argv[i]), end='')
+            print()
+    else:
+        print(".")
         
