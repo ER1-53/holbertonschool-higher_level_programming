8e6f5b8ebf59de062ec7638d021f60d277510eee
ER1-53 <cremey.erwan@gmail.com>
2023-09-29 17:17:15 +0200

    test for task my_calculator

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -7,13 +7,13 @@ if __name__ == "__main__":
     if argc != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    if argv[2] == '+':
+    if argv[2] == "+":
         print("{}".format(calc.add(int(argv[1]), int(argv[3]))))
-    elif argv[2] == '-':
+    elif argv[2] == "-":
         print("{}".format(calc.sub(int(argv[1]), int(argv[3]))))
     elif argv[2] == '*':
         print("{}".format(calc.mul(int(argv[1]), int(argv[3]))))
-    elif argv[2] == '/':
+    elif argv[2] == "/":
         print("{}".format(calc.div(int(argv[1]), int(argv[3]))))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
