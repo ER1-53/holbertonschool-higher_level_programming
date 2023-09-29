07123f01bb990eaa3f4e3694c5db744bf30ba065
ER1-53 <cremey.erwan@gmail.com>
2023-09-29 14:15:42 +0200

    argv module

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -2,7 +2,7 @@
 import calculator_1 as calc
 from sys import argv
 if __name__ == "__main__":
-    argv = sys.argv
+    argv = argv
     argc = len(argv)
     if argc != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
