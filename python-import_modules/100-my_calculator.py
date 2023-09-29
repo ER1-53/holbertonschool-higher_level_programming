2e0f6f22287853521bf2e6288ee93dc6db931547
ER1-53 <cremey.erwan@gmail.com>
2023-09-29 14:12:47 +0200

    modification L12 simple quote

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
         print("{}".format(calc.add(int(argv[1]), int(argv[3]))))
     elif argv[2] == '-':
         print("{}".format(calc.sub(int(argv[1]), int(argv[3]))))
-    elif argv[2] == "*":
+    elif argv[2] == '*':
         print("{}".format(calc.mul(int(argv[1]), int(argv[3]))))
     elif argv[2] == '/':
         print("{}".format(calc.div(int(argv[1]), int(argv[3]))))
