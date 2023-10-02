9557501ebb6d5e02464040cd5b73a843d2981523
ER1-53 <cremey.erwan@gmail.com>
2023-10-02 16:36:22 +0200

    update task 6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for element in row:
-            if element - 1 != None:
+            if element != None:
                 print("{:d}".format(element), end=" ")
             else:
                 print("{:d}".format(element))
