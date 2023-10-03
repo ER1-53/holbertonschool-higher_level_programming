fa9c38f1357f05566d0580d1a37e62a03d008be2
ER1-53 <cremey.erwan@gmail.com>
2023-10-03 16:50:15 +0200

    creat 9 max interger

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return 0, None
+    for i in range(len(my_list)):
+        for j in range(i + 1, len(my_list)):
+            if my_list[i] < my_list[j]:
+                i = j
+        return my_list[i]
