882553173219cf244c85b140923cf7f8dc26b1e3
ER1-53 <cremey.erwan@gmail.com>
2023-10-03 15:36:13 +0200

    modif condition >=

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -2,7 +2,7 @@
 def replace_in_list(my_list, idx, element):
     if idx < 0:
         return None
-    if idx > len(my_list) - 1:
+    if idx >= len(my_list):
         return my_list
     my_list[idx] = element
     return my_list
