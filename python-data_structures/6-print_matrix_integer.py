2ccc7cd59e0006c66af2c56495357594c3e28075
ER1-53 <cremey.erwan@gmail.com>
2023-10-03 09:55:30 +0200

    update task 6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for element in row:
-            if element != None:
-                print("{:d}".format(element), end=" ")
-            else:
-                print("{:d}".format(element))
+    for row in range(len(matrix)):
+        for element in range(len(matrix[0])):
+            print("{:d}".format(matrix[row][element]), end="")
+            if element < len(matrix[0]) - 1:
+                print(" ",end="")
         print()
