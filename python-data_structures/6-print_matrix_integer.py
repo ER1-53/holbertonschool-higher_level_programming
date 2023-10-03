1ce9bf5d60906ecc1273f166cb732fd099556d02
ER1-53 <cremey.erwan@gmail.com>
2023-10-03 15:25:06 +0200

    pystyle correction task 1 & 6 & 7

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -4,5 +4,5 @@ def print_matrix_integer(matrix=[[]]):
         for element in range(len(matrix[0])):
             print("{:d}".format(matrix[row][element]), end="")
             if element < len(matrix[0]) - 1:
-                print(" ",end="")
+                print(" ", end="")
         print()
