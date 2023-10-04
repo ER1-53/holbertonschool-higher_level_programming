5272f3e0f1f07ba0497a3b9947b3303dbda222bd
ER1-53 <cremey.erwan@gmail.com>
2023-10-04 18:24:11 +0200

    error of write

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_dic = {}
+    new_dico = {}
     for key, value in a_dictionary.items():
         new_dico[key] = value * 2
-    return new_dic
+    return new_dico
