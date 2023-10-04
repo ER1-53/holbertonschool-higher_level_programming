bb76eccc930a95a972c1d78d3c3a880abe7fd897
ER1-53 <cremey.erwan@gmail.com>
2023-10-04 18:22:59 +0200

    creat task 9

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_dico = {}
+    new_dic = {}
     for key, value in a_dictionary.items():
         new_dico[key] = value * 2
-    return new_dico
+    return new_dic
