10454341e5a0610750ef797ff41ec096c8b0f75a
ER1-53 <cremey.erwan@gmail.com>
2023-10-05 22:08:29 +0200

    restor None

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if len(my_list) == 0:
+    if my_list is None:
         return None
     for i in range(len(my_list)):
         for j in range(i + 1, len(my_list)):
