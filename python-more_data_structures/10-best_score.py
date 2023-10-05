85cee9ebc15464609d3784433a2f8d537e9497c5
ER1-53 <cremey.erwan@gmail.com>
2023-10-05 21:50:22 +0200

    update task 10

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,6 +2,6 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    sorted_dict = sorted(a_dictionary.items())
-    best_key, value = sorted_dict[0]
+    sorted_dict = sorted(a_dictionary.items(), key=lambda x: x[1])
+    best_key, best_value = sorted_dict[-1]
     return best_key
