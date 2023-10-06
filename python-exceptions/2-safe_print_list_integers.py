f523c751dec93ae43ac6a0b36d6a4cc228a913eb
ER1-53 <cremey.erwan@gmail.com>
2023-10-06 10:15:30 +0200

    creat task 2

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    try:
+        for element in range(x):
+            if type(my_list[element]) == int:
+                print("{:d}".format(my_list[element]), end='')
+                count += 1
+    except IndexError:
+        pass
+    finally:
+        print()
+        return count
