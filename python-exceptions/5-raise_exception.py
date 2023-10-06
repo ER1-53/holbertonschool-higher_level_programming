b694bf562ed61332c1ebd162e2a5132b5c79b302
ER1-53 <cremey.erwan@gmail.com>
2023-10-06 15:50:40 +0200

    code style

--- python-exceptions/5-raise_exception.py
+++ python-exceptions/5-raise_exception.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def raise_exception():
     proposition = [1, 'think']
-    result = proposition[0] + proposition[1]
-    return result
+    resultat = proposition[0] + proposition[1]
+    return resultat
