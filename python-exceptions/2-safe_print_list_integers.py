63789f39e60498e8f64c4c997503e63249a54bc4
ER1-53 <cremey.erwan@gmail.com>
2023-10-06 16:27:23 +0200

    test for correction

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -6,8 +6,9 @@ def safe_print_list_integers(my_list=[], x=0):
             if type(my_list[element]) == int:
                 print("{:d}".format(my_list[element]), end='')
                 count += 1
-    except IndexError:
+    except ValueError:
         pass
+    except IndexError:
+         print("Traceback (most recent call last):")
     finally:
-        print()
         return count
