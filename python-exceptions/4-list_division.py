541f16d923c6e7bbb27448b1b1c12a1399d4348f
ER1-53 <cremey.erwan@gmail.com>
2023-10-06 15:05:32 +0200

    default keyword

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -6,7 +6,7 @@ def list_division(my_list_1, my_list_2, list_length):
             div_result = my_list_1[num] / my_list_2[num]
             result.append(div_result)
         except IndexError:
-            print("out of range0")
+            print("out of range")
             result.append(0)
         except TypeError:
             print("wrong type")
