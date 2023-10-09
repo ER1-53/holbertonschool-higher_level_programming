34f31486fba3165f30c97e0faf900202878e9a54
ER1-53 <cremey.erwan@gmail.com>
2023-10-09 15:12:24 +0200

    comment trie again

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -12,6 +12,7 @@ It's class Square for creat a square.
 :param attribut: Description de l'attribut
     """
     def __init__(self,size):
+            
             """
             It's class Square for creat a square.
             :param attribut: Description de l'attribut
