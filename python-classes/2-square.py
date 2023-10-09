89adbecac3fc461e69ea7f97e4efbfbdbd4ccf44
ER1-53 <cremey.erwan@gmail.com>
2023-10-09 15:54:08 +0200

    correction of comment

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+It's class Square for creat a square.
+"""
+
+
 class Square:
     """
     It's class Square for creat a square.
@@ -7,11 +12,12 @@ class Square:
     def __init__(self, size=0):
 
         """
-    Calcule et renvoie l'aire d'un rectangle.
-    :param length: Longueur du rectangle.
-    :param width: Largeur du rectangle.
-    :return: L'aire du rectangle.
+        Initializes a new Square object with a given size.
+        :param size: The side length of the square.
+        :raises ValueError: If size is less than 0.
+        :raises TypeError: If size is not an integer.
         """
+
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
