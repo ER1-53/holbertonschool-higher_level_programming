502a3dbc363372526d733bfdd591e9cbbeeb49c7
ER1-53 <cremey.erwan@gmail.com>
2023-10-09 11:17:43 +0200

    comment style

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
+"""
+It's class Square for creat a square.
+:param attribut: Description de l'attribut
+"""
+
 class Square:
 
     """
-    It's class Square for creat a square.
-
-    :param attribut: Description de l'attribut
+It's class Square for creat a square.
+:param attribut: Description de l'attribut
     """
