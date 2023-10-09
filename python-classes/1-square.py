20fb58fded56a626f7bb160fca3121b1e080e9c9
ER1-53 <cremey.erwan@gmail.com>
2023-10-09 15:06:19 +0200

    square task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+It's class Square for creat a square.
+:param attribut: Description de l'attribut
+"""
+
+
+class Square:
+
+    """
+It's class Square for creat a square.
+:param attribut: Description de l'attribut
+    """
+    def __init__(self,size):
+        Square.__size = size
