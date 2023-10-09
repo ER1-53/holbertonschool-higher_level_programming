e476dd570fedcb10b2220310f70244a14b5ad2e7
ER1-53 <cremey.erwan@gmail.com>
2023-10-09 21:43:06 +0200

    update task 4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -21,14 +21,32 @@ class Square:
         self.__size = size
 
     def area(self):
+
+        """
+        Initializes a area of the Square.
+        with calcul self.size * self.size
+        """
+
         return self.__size ** 2
 
     @property
     def size(self):
+
+        """
+        With property is the solution for modif value
+        Property is for getter
+        """
+
         return self.__size
 
     @size.setter
     def size(self, value):
+
+        """
+        With .setter is the solution for modif value
+        @instance.setter is for setter
+        """
+
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
