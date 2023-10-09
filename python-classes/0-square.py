5a6bfb0f7d20e7ca39efe36571805a466d2e0778
ER1-53 <cremey.erwan@gmail.com>
2023-10-09 11:12:25 +0200

    update 0-square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -5,4 +5,15 @@ class Square:
 
     :param attribut: Description de l'attribut
     """
+    def __init__(self, attribu):
+
+        """My addition function
+
+            Args:
+                a: first integer
+                b: second integer
+
+            Returns:
+                The return value. a + b
+        """
 
