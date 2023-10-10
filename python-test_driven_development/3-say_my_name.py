151167ed97a92c2954c12957d40d2f20f8b7a722
ER1-53 <cremey.erwan@gmail.com>
2023-10-10 22:26:53 +0200

    last correction task 3

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,10 +1,24 @@
 #!/usr/bin/python3
+
+"""
+A function that says the full name.
+"""
+
+
 def say_my_name(first_name, last_name=""):
+    """
+    Print the full name by combining first_name and last_name.
+
+    :param first_name: The first name (a string).
+    :param last_name: The last name (a string, optional,
+    default is an empty string).
+    :return: None
+    """
 
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
+    if not first_name and not last_name:
+        return None
     print("My name is {} {}".format(first_name, last_name))
-
-    return
