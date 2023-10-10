5485da4747d4f153c431cdcf889d2123ffcead25
ER1-53 <cremey.erwan@gmail.com>
2023-10-10 20:56:29 +0200

    all test ok

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -12,14 +12,12 @@ def matrix_divided(matrix, div):
     :param div: The divisor (number).
     :return: A new matrix with the results of the division.
     """
-
+    error_M = "matrix must be a matrix (list of lists) of integers/floats"
     if not isinstance(matrix, list) or len(matrix) == 0 or \
             not all(isinstance(row, list) for row in matrix):
-        raise TypeError("matrix must be a matrix \
-            list of lists) of integers/floats")
+        raise TypeError(error_M)
     elif any(len(row) != len(matrix[0]) for row in matrix):
-        raise TypeError("Each row of the matrix \
-             must have the same size")
+        raise TypeError("Each row of the matrix must have the same size")
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
     elif div == 0:
