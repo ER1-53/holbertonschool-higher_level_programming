cac0f30986aed50898a7c0f939cc10509794601d
ER1-53 <cremey.erwan@gmail.com>
2023-10-11 14:52:00 +0200

    modif task 6 import function

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -4,7 +4,7 @@
 
 
 import unittest
-
+max_integer = __import__('6-max_integer').max_integer
 
 class test_unit(unittest.TestCase):
     """Function to find and return the max integer in a list of integers
@@ -12,25 +12,26 @@ class test_unit(unittest.TestCase):
     """
     def test_max_int(self):
         numbers = [1, 2, 3, 4]
-        max_value = max(numbers)
+        max_value = max_integer(numbers)
         self.assertEqual(max_value, 4)
 
     def test_max_with_neg(self):
         numbers = [-1, -2, -3, -4]
-        max_value = max(numbers)
+        max_value = max_integer(numbers)
         self.assertEqual(max_value, -1)
 
     def test_max_int_empty_list(self):
         numbers = []
-        max_value = None
+        max_value = max_integer(numbers)
         self.assertEqual(max_value, None)
 
     def test_str_fail(self):
         numbers = ["poupy", 4]
         with self.assertRaises(TypeError):
-            max_value = max(numbers)
+            max_value = max_integer(numbers)
 
 
-if __name__ == '__main__':
-    unittest.main()
-    
+    def test_max_allsame(self):
+            numbers = [4, 4, 4, 4]
+            max_value = max_integer(numbers)
+            self.assertEqual(max_value, 4)
