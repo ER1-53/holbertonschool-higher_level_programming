54582047423a808f7e0e33e613f5b6695f248f74
ER1-53 <cremey.erwan@gmail.com>
2023-10-11 16:32:07 +0200

    update task 1 with value

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -13,17 +13,29 @@ Args:
 - height (int): The height of the rectangle (default is 0).
     """
     def __init__(self, width=0, height=0):
-        self.width = width
-        self.height = height
+        self.__width = width
+        self.__height = height
 
+    @property
     def width(self):
-        if not isinstance(self.width, int):
-            raise TypeError("width must be an integer")
-        if self.width < 0:
-            raise ValueError("width must be >= 0")
+        return self.__width
 
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__height
+
+    @property
     def height(self):
-        if not isinstance(self.height, int):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        if self.height < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
+        self.__height
