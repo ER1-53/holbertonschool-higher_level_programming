c892d90d5fa58c48634b62a60e8e1b8edbc3e0c6
ER1-53 <cremey.erwan@gmail.com>
2023-10-11 15:41:48 +0200

    creat all file & task 0

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+It's class Rectangle for creat a rectangle.
+"""
+
+
+class Rectangle:
+
+    """
+        Initializes a new Rectangle.
+
+        Args:
+        - width (int): The width of the rectangle (default is 0).
+        - height (int): The height of the rectangle (default is 0).
+    """
