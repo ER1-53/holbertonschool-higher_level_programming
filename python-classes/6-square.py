db089e906e278d9d10df9bebde2d37160c203fe1
ER1-53 <cremey.erwan@gmail.com>
2023-10-11 18:53:01 +0200

    add condition in def init

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -14,7 +14,16 @@ class Square:
         """
 
         self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        
         self.__position = position
+        if not isinstance(position, tuple) or len(position) != 2 or \
+            not isinstance(position[0], int) or not isinstance(position[1], int) or \
+                not all(i >= 0 for i in position):
+            raise TypeError("position must be a tuple of 2 positive integers")
 
     @property
     def size(self):
