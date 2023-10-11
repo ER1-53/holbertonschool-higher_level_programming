155d8c89faee3257466c584f7c55fdc988821358
ER1-53 <cremey.erwan@gmail.com>
2023-10-11 15:46:10 +0200

    style correction

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -7,9 +7,8 @@ It's class Rectangle for creat a rectangle.
 class Rectangle:
 
     """
-        Initializes a new Rectangle.
-
-        Args:
-        - width (int): The width of the rectangle (default is 0).
-        - height (int): The height of the rectangle (default is 0).
+Initializes a new Rectangle.
+Args:
+- width (int): The width of the rectangle (default is 0).
+- height (int): The height of the rectangle (default is 0).
     """
