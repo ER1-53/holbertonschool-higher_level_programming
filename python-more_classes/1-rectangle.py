a6f533f4e329eb4c17a1d62f4403c92066e9dbe4
ER1-53 <cremey.erwan@gmail.com>
2023-10-11 15:53:00 +0200

    creat task 1

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -7,9 +7,23 @@ It's class Rectangle for creat a rectangle.
 class Rectangle:
 
     """
-        Initializes a new Rectangle.
-
-        Args:
-        - width (int): The width of the rectangle (default is 0).
-        - height (int): The height of the rectangle (default is 0).
+Initializes a new Rectangle.
+Args:
+- width (int): The width of the rectangle (default is 0).
+- height (int): The height of the rectangle (default is 0).
     """
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    def width(self):
+        if not isinstance(self.width, int):
+            raise TypeError("width must be an integer")
+        if self.width < 0:
+            raise ValueError("width must be >= 0")
+
+    def height(self):
+        if not isinstance(self.height, int):
+            raise TypeError("height must be an integer")
+        if self.height < 0:
+            raise ValueError("height must be >= 0")
