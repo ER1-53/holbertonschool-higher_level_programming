65e062cf165d4683d08048b0522f78e76fc60268
ER1-53 <cremey.erwan@gmail.com>
2023-10-17 19:20:32 +0200

    modif of th condition L14 more restrictiv

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -6,11 +6,12 @@
 class BaseGeometry:
     """A function that checks if an object is an instance of
 the specified class or a subclass of it."""
+
     def area(self):
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
