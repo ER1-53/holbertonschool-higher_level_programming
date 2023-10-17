6ff16ecc1adfc097c028c2d0447a6e179ad44f79
ER1-53 <cremey.erwan@gmail.com>
2023-10-17 14:22:42 +0200

    description

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+
+"""function that prints the list of attribut and methode"""
+
+
 def lookup(obj):
+    """function that prints the list of attribut and methode"""
+
     list = dir(obj)
     return list
