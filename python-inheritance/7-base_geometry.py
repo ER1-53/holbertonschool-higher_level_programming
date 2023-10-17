af3a09b54ab7eff7d47d2590feaf5b60072fdc3e
ER1-53 <cremey.erwan@gmail.com>
2023-10-17 18:32:08 +0200

    over-indented L13 to L16

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -10,7 +10,7 @@ the specified class or a subclass of it."""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-            if not isinstance(value, int):
-                raise TypeError("{} must be an integer".format(name))
-            if value <= 0:
-                raise ValueError("{} must be greater than 0".format(name))
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
