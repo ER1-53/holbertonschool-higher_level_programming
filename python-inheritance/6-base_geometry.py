9ae97a675221c78d7fa69209eeebf6a85e6dcb1e
ER1-53 <cremey.erwan@gmail.com>
2023-10-17 17:51:29 +0200

    creat task 6

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+
+"""Base geometry class"""
+
+
+class BaseGeometry:
+    """A function that checks if an object is an instance of
+the specified class or a subclass of it."""
+    def area(self):
+        raise Exception("area() is not implemented")
