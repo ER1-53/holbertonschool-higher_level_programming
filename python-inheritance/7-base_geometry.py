2818351d71e0503ae0d38f8ab95b42136fba6f50
ER1-53 <cremey.erwan@gmail.com>
2023-10-17 18:06:51 +0200

    creat task 7

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+
+"""Base geometry class"""
+
+
+class BaseGeometry:
+    """A function that checks if an object is an instance of
+the specified class or a subclass of it."""
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+            if not isinstance(value, int):
+                raise TypeError("<name> must be an integer")
+            if value <= 0:
+                raise ValueError("<name> must be greater than 0")
