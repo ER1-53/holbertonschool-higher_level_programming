064574d21ee8b019f6867b635c904edc2fd36dc0
ER1-53 <cremey.erwan@gmail.com>
2023-10-17 15:56:37 +0200

    update descript

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
 
-"""function that prints the list of
-attribut and methode"""
+"""A function that checks if an object is an instance of a specific class."""
+
 
 
 def is_same_class(obj, a_class):
-    """function that prints the list of
-attribut and methode"""
+    """
+    Check if the provided object is an instance of the specified class.
+
+    Args:
+        obj: The object to check.
+        a_class: The class to compare against.
+
+    Returns:
+        True if the object is an instance of the specified class, False otherwise.
+    """
 
     return type(obj) is a_class
