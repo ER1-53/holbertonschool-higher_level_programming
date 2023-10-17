1579af5c3567f229d8ae7d5c088835008f0d0cd1
ER1-53 <cremey.erwan@gmail.com>
2023-10-17 16:34:02 +0200

    test other possibility

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -8,7 +8,6 @@ def is_kind_of_class(obj, a_class):
     """function that prints the list of
 attribut and methode"""
 
-    print(type(obj))
-    if type(obj) is a_class or issubclass(type(obj), a_class):
-        return True
-    return False
+    if not isinstance(obj, a_class) and not issubclass(type(obj), a_class):
+        return False
+    return True
