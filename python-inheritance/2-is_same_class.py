79bd0c2d77f552103085ddac59fdc5ab2af7e993
ER1-53 <cremey.erwan@gmail.com>
2023-10-17 15:54:38 +0200

    creat task 2

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+
+"""function that prints the list of
+attribut and methode"""
+
+
+def is_same_class(obj, a_class):
+    """function that prints the list of
+attribut and methode"""
+
+    return type(obj) is a_class
