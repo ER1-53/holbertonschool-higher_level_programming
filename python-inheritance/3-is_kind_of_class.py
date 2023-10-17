9e448bc4e9d32e8ba883f301668c606222200255
ER1-53 <cremey.erwan@gmail.com>
2023-10-17 16:45:17 +0200

    update description

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python3
 
-"""function that prints the list of
-attribut and methode"""
+"""A function that checks if an object is an instance or a subclass of a specific class."""
 
 
 def is_kind_of_class(obj, a_class):
-    """function that prints the list of
-attribut and methode"""
+    
+    """
+    Check if the provided object is an instance of the specified class or a subclass of it.
+
+    Args:
+        obj: The object to check.
+        a_class: The class to compare against.
+
+    Returns:
+        True if the object is an instance of the specified class or a subclass of it, False otherwise.
+    """
 
     if not isinstance(obj, a_class) and not issubclass(type(obj), a_class):
         return False
