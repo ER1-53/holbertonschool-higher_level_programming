37d280ec0d0053c6ec885c562ae2393dc7e04cb4
ER1-53 <cremey.erwan@gmail.com>
2023-10-17 20:59:55 +0200

    easy code task 8

--- python-inheritance/8-main.py
+++ python-inheritance/8-main.py
@@ -12,6 +12,6 @@ except Exception as e:
     print("[{}] {}".format(e.__class__.__name__, e))
 
 try:
-    r2 = Rectangle(4, True)
+    r2 = Rectangle(True, 0)
 except Exception as e:
     print("[{}] {}".format(e.__class__.__name__, e))
