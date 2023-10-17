ef8af19c2a490044d582f1bede8fe2ef4d8ada0e
ER1-53 <cremey.erwan@gmail.com>
2023-10-17 16:04:56 +0200

    creat task 3

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+
+"""function that prints the list of
+attribut and methode"""
+
+
+def is_kind_of_class(obj, a_class):
+    """function that prints the list of
+attribut and methode"""
+
+    print(type(obj))
+    if not isinstance(obj, a_class):
+        return False
+    return True
