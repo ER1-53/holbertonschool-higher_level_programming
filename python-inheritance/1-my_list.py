513395c968a8332ac39d97956b6d1106010c814e
ER1-53 <cremey.erwan@gmail.com>
2023-10-17 15:46:54 +0200

    update style

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -3,13 +3,12 @@
 """A custom list class that can print itself sorted."""
 
 
-
 class MyList(list):
 
     """that prints the list, but sorted (ascending sort)"""
 
     def print_sorted(self):
-        if not all(isinstance (x, int)for x in self):
+        if not all(isinstance(x, int) for x in self):
             raise TypeError("Not all elements in the list are integers")
         sorted_list = sorted(self)
         print(sorted_list)
