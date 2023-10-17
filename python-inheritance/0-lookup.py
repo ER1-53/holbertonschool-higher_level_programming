68cc68392971754c0b01f7b741bb5b0228bc21f1
ER1-53 <cremey.erwan@gmail.com>
2023-10-17 14:25:18 +0200

    pystyle

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
-"""function that prints the list of attribut and methode"""
+"""function that prints the list of 
+attribut and methode"""
 
 
 def lookup(obj):
-    """function that prints the list of attribut and methode"""
+    """function that return the list object"""
 
     list = dir(obj)
     return list
