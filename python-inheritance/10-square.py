a13e4f13313a3bb50d85a40e73746717d3baf8ec
ER1-53 <cremey.erwan@gmail.com>
2023-10-18 08:19:03 +0200

    creat task 10 & 11

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""Base geometry class"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """A function that checks if an object is an instance of
+the specified class or a subclass of it."""
+    def __init__(self, size):
+        self.__size = size
+        super().__init__(size, size)
+    
--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""Base geometry class"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """A function that checks if an object is an instance of
+the specified class or a subclass of it."""
+    def __init__(self, size):
+        self.__size = size
+        super().__init__(size, size)
+    
+    def __str__(self):
+        return f"[Square] {self.__size}/{self.__size}"
+    
