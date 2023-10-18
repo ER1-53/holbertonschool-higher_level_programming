1cbbd87d4d6c1e2a0256bfeddaed00801ae5624b
ER1-53 <cremey.erwan@gmail.com>
2023-10-18 13:06:30 +0200

    creat task 2

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+""" function for write file"""
+
+
+def append_write(filename="", text=""):
+    """ function for write file"""
+    with open(filename, 'a', encoding="utf-8") as file:
+        return file.write(text)
