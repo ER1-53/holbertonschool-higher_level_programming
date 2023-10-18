3b96414c3e7c792e4aa4b3e7b85b6e77dcb11d62
ER1-53 <cremey.erwan@gmail.com>
2023-10-18 07:36:09 +0200

    creat task 9

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+
+"""Base geometry class"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """A function that checks if an object is an instance of
+the specified class or a subclass of it."""
+
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"
