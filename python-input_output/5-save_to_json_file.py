b5ad201d68d8555b33e5ee7c82c79552c22d9cd9
ER1-53 <cremey.erwan@gmail.com>
2023-10-18 14:42:32 +0200

    creat task 5

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+""" function for write file"""
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """ function for write file"""
+    with open(filename, 'a', encoding="utf-8") as file:
+        json.dump(my_obj, file)
