101bf6682ddc0502eb37dea08d1f81f59ac8df61
ER1-53 <cremey.erwan@gmail.com>
2023-10-18 15:04:53 +0200

    creat task 6

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
-def read_file(filename=""):
-    with open('filename', 'r',  encoding="utf-8") as file:
-        text = file.read()
-    
+""" function for write file"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """ function for write file"""
+    with open(filename, 'r', encoding="utf-8") as file:
+        return json.load(file)
