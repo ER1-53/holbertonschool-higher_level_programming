721026d46f41ed7d072c2b0f3fd7e20e402c4303
ER1-53 <cremey.erwan@gmail.com>
2023-10-18 12:53:57 +0200

    creat task1

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+""" function for write file"""
+
+
+def write_file(filename="", text=""):
+    """ function for write file"""
+    with open(filename, 'w', encoding="utf-8") as file:
+        return file.write(text)
