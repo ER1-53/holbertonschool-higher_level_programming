d69097d1434389c72c56cd0689301ca5609f3059
ER1-53 <cremey.erwan@gmail.com>
2023-10-19 22:25:27 +0200

    update style

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-""" function for write file"""
+"""function for write file"""
 import json
 
 
 def class_to_json(obj):
-    """ function for write file"""
-
+    """function for write file"""
     return obj.__dict__
