936c5b2401a5c41d3535df6e5002e74fd99fce61
ER1-53 <cremey.erwan@gmail.com>
2023-10-19 20:59:47 +0200

    creat task 7 & update task 5

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -7,5 +7,5 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """ function for write file"""
-    with open(filename, 'a', encoding="utf-8") as file:
+    with open(filename, 'w', encoding="utf-8") as file:
         json.dump(my_obj, file)
--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+""" function for write file"""
+import sys
+import json
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+
+try:
+    data = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    data = []
+
+data.extend(sys.argv[1:])
+save_to_json_file(data, "add_item.json")
