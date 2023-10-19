3e79e9b785627e815d99a7382c46167ccd1ffe45
ER1-53 <cremey.erwan@gmail.com>
2023-10-19 22:21:56 +0200

    creat task 8 & 9 & 10

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -6,7 +6,7 @@ class Student:
     """Init with first name, last name & age"""
 
     def __init__(self, first_name, last_name, age):
-        self.fist_name = first_name
+        self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+""" function for write file"""
+import json
+
+
+def class_to_json(obj):
+    return obj.__dict__
