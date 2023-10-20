fbfd4d107489e348168d63681283951c9951cf26
ER1-53 <cremey.erwan@gmail.com>
2023-10-20 19:36:54 +0200

    style update

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -9,12 +9,12 @@ def pascal_triangle(n):
 
     triangle = []
     for i in range(n):
-        row = [1]  
+        row = [1]
         if triangle:
-            prev_row = triangle[-1]  
+            prev_row = triangle[-1]
             for j in range(len(prev_row) - 1):
-                row.append(prev_row[j] + prev_row[j + 1])  
-            row.append(1) 
+                row.append(prev_row[j] + prev_row[j + 1])
+            row.append(1)
         triangle.append(row)
 
     return triangle 
