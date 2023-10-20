e08d89ac8ad3e939583df6593ba7313a455b3bdd
ER1-53 <cremey.erwan@gmail.com>
2023-10-20 19:33:43 +0200

    creat task 12

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -4,12 +4,17 @@ import json
 
 
 def pascal_triangle(n):
-
-    list = []
-    
     if n <= 0:
-        return list
-    
-    for x in range(1, n):
-        list.extend[1:] 
-    
+        return []
+
+    triangle = []
+    for i in range(n):
+        row = [1]  
+        if triangle:
+            prev_row = triangle[-1]  
+            for j in range(len(prev_row) - 1):
+                row.append(prev_row[j] + prev_row[j + 1])  
+            row.append(1) 
+        triangle.append(row)
+
+    return triangle 
