780468f4df9638a35fa3c24909487826fb486664
ER1-53 <cremey.erwan@gmail.com>
2023-10-20 10:31:16 +0200

    creat task 11

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+""" Class of student"""
+
+
+class Student:
+    """Init with first name, last name & age"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {attr: getattr(self, attr) for attr in attrs
+                    if hasattr(self, attr)}
+
+    def reload_from_json(self, json):
+        for obj in json:
+            self.__dict__[obj] = json[obj]
