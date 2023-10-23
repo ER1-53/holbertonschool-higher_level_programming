1f6330953a612719051942d1f4f092b1f2421b91
ER1-53 <cremey.erwan@gmail.com>
2023-10-23 15:52:14 +0200

    update Rectangle task 3

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -12,3 +12,48 @@ class Rectangle(Base):
         self.__height = height
         self.__x = x
         self.__y = y
+        
+        attributes = ["width", "height", "x", "y"]
+
+        for attr in attributes:
+            value = locals()[attr]
+            if not isinstance(value, int):
+                raise TypeError(f"{attr} must be an integer")
+            elif (attr == "width" or attr == "height") and value <= 0:
+                raise ValueError(f"{attr} must be > 0")
+            elif (attr == "y" or attr == "x") and value < 0:
+                raise ValueError(f"{attr} must be >= 0")
+   
+
+        @property
+        def width(self):
+            return self.__width
+        
+        @width.setter
+        def width(self, value):
+            self.__width = value
+            return value
+        
+        @property
+        def height(self):
+            return self.__height
+        @height.setter
+        def height(self, value):
+            self.__height = value
+            return value
+        
+        @property
+        def x(self):
+            return self.__x
+        @x.setter
+        def x(self, value):
+            self.__x = value
+            return value
+        
+        @property
+        def y(self):
+            return self.__y
+        @y.setter
+        def y(self, value):
+            self.__y = value
+            return value
