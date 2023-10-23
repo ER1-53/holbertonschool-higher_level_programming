2ce5e9732be4fb2af6f88d6bd3b0966beb224351
ER1-53 <cremey.erwan@gmail.com>
2023-10-23 09:41:22 +0200

    creat readme, base, rectangle & init task 1

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Base of the project"""
+
+
+class Base:
+    """ function for read file"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        Base.__nb_objects += 1
+        if id:
+            self.id = id
+        else:
+            self.id = Base.__nb_objects
