f5de7b190ffeafc6945697161798848993b2da19
ER1-53 <cremey.erwan@gmail.com>
2023-10-23 14:09:36 +0200

    creat rectangle & descript base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -3,7 +3,7 @@
 
 
 class Base:
-    """ function for read file"""
+    """ base fonction"""
     __nb_objects = 0
 
     def __init__(self, id=None):
---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Rectangle of the project"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """Class Rectangle"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(id)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
