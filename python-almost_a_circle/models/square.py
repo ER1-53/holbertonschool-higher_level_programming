4fbf3829a60cf2b64afd34cce82f8efb4ea35c4f
ER1-53 <cremey.erwan@gmail.com>
2023-10-24 13:00:28 +0200

    update square task 11

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -7,4 +7,13 @@ class Square(Rectangle):
     """Class Square"""
 
     def __init__(self, size, x=0, y=0, id=None):
-        super().__init__(size, size, x, y, id)
+        super().__init__(size, size, x, y, id)    
+
+    @property
+    def size(self):
+        return self.height
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
