1d26b0c604d1ab8e1aa7dc21f5e73e6eb2ed7c83
ER1-53 <cremey.erwan@gmail.com>
2023-10-24 17:21:24 +0200

    update style

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -26,7 +26,9 @@ class Base:
     @classmethod
     def save_to_file(cls, list_objs):
         """writes the JSON string representation"""
+        
         filename = cls.__name__ + ".json"
         list_objs = [obj.to_dictionary() for obj in list_objs]
+        
         with open(filename, 'w', encoding='utf8') as file:
             json.dump(list_objs, file)
