45bfd416f0caca9c76cf0f18d93517d7e52eda56
ER1-53 <cremey.erwan@gmail.com>
2023-10-24 11:34:43 +0200

    creat square

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Square of the project"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Class Square"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
