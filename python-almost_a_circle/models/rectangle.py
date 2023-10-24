68e752ddd553bf6407ccd1dbbe0aa94b0cfa8dde
ER1-53 <cremey.erwan@gmail.com>
2023-10-24 10:56:35 +0200

    update style

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -115,10 +115,9 @@ class Rectangle(Base):
         h = self.height
         # adaptation for pystylecode
         return f"[{class_name}] ({id}) {self.x}/{self.y} - {w}/{h}"
-    
+
     def update(self, *args):
         """Update all attribut """
         attribute_names = ['id', 'width', 'height', 'x', 'y']
-
         for i in range(min(len(args), len(attribute_names))):
             setattr(self, attribute_names[i], args[i])
