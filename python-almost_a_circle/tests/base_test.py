9f4a88869971ae50bcc30918635960443d7ff756
ER1-53 <cremey.erwan@gmail.com>
2023-10-25 16:05:40 +0200

    test bis Na

--- python-almost_a_circle/tests/base_test.py
+++ python-almost_a_circle/tests/base_test.py
@@ -5,36 +5,91 @@ import unittest
 from models.base import Base
 from models.rectangle import Rectangle
 from models.square import Square
+import json
+import os
+import sys
+from io import StringIO
 
+class TestBase(unittest.TestCase):
+    def test_creation_with_id(self):
+        #Checks if an object is created with the specified ID.
+        obj = Base(1)
+        self.assertEqual(obj.id, 1)
 
-class Test_Base(unittest.TestCase):
-    """Function to find and return the max integer in a list of integers
-        If the list is empty, the function returns None
-    """
-    
-    def test_init(self):
-        b = Base()
-        self.assertEqual(b.id, 1)
+    def test_creation_without_id(self):
+        #Vérifie que la classe Base attribue automatiquement des IDs
+        # incrémentiels aux objets créés.
+        obj1 = Base()
+        obj2 = Base()
+        self.assertEqual(obj1.id, 1)
+        self.assertEqual(obj2.id, 2)
 
-    def test_init_with_custom_id(self):
-        b = Base(100)
-        self.assertEqual(b.id, 100)
+    def test_private_nb_objects_no_direct_access(self):
+        #S'assure qu'il n'y a pas d'accès direct à l'attribut privé
+        # __nb_objects.
+        obj1 = Base()
+        with self.assertRaises(AttributeError):
+            obj1.__nb_objects
 
-    def test_init_with_neg_custom_id(self):
-        b = Base(-5)
-        self.assertEqual(b.id, -5)
+    #Vérifient que la méthode to_json_string convertit correctement
+    # des données en format JSON.
+    def test_to_json_string_with_data(self):
+        data = [{'key1': 'value1', 'key2': 'value2'}]
+        json_str = Base.to_json_string(data)
+        self.assertEqual(json_str, json.dumps(data))
 
-    def test_init_with_string_custom_id(self):
-        b = Base("holbies")
-        self.assertEqual(b.id, "holbies")
+    def test_to_json_string_with_empty_data(self):
+        data = []
+        json_str = Base.to_json_string(data)
+        self.assertEqual(json_str, "[]")
 
-    def test_init_with_char_custom_id(self):
-        b = Base("H")
-        self.assertEqual(b.id, "H")
+    def test_to_json_string_with_none_data(self):
+        json_str = Base.to_json_string(None)
+        self.assertEqual(json_str, "[]")
 
-    def test_init_with_float_custom_id(self):
-        b = Base(8.5)
-        self.assertEqual(b.id, 8.5)
+    #Testent la sauvegarde de données JSON dans un fichier et leur lecture
+    # pour vérifier l'intégrité des données.
 
-if __name__ == '__main__':
+    def test_save_to_file_with_empty_data(self):
+        data = []
+        filename = "Base.json"
+
+        Base.save_to_file(data)
+
+        with open(filename, 'r', encoding='utf-8') as file:
+            saved_data = json.load(file)
+            self.assertEqual(saved_data, [])
+
+        os.remove(filename)
+
+    def test_save_to_file_with_none_data(self):
+        filename = "Base.json"
+
+        Base.save_to_file(None)
+
+        with open(filename, 'r', encoding='utf-8') as file:
+            saved_data = json.load(file)
+            self.assertEqual(saved_data, [])
+
+        os.remove(filename)
+
+    #Vérifient que la méthode from_json_string peut convertir une chaîne
+    # JSON en une structure de données Python.
+
+    def test_from_json_string_with_data(self):
+        data = [{'key1': 'value1', 'key2': 'value2'}]
+        json_str = json.dumps(data)
+        loaded_data = Base.from_json_string(json_str)
+        self.assertEqual(loaded_data, data)
+
+    def test_from_json_string_with_empty_data(self):
+        json_str = "[]"
+        loaded_data = Base.from_json_string(json_str)
+        self.assertEqual(loaded_data, [])
+
+    def test_from_json_string_with_none_data(self):
+        loaded_data = Base.from_json_string(None)
+        self.assertEqual(loaded_data, [])
+
+if __name__ == "__main__":
     unittest.main()
