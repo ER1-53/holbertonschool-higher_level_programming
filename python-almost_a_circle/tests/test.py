84eb9ae31c9c04b6383c6792829007f3a2095c8d
ER1-53 <cremey.erwan@gmail.com>
2023-10-25 14:56:21 +0200

    task 0 test

---
+++ python-almost_a_circle/tests/test.py
@@ -0,0 +1,126 @@
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
+
+# -----------------------------------------------------------------#
+
+class Test_Base(unittest.TestCase):
+    def test_init(self):
+        b = Base()
+        self.assertEqual(b.id, 1)
+
+    def test_init_with_custom_id(self):
+        b = Base(100)
+        self.assertEqual(b.id, 100)
+
+    def test_init_with_neg_custom_id(self):
+        b = Base(-5)
+        self.assertEqual(b.id, -5)
+
+    def test_init_with_string_custom_id(self):
+        b = Base("holbies")
+        self.assertEqual(b.id, "holbies")
+
+    def test_init_with_char_custom_id(self):
+        b = Base("H")
+        self.assertEqual(b.id, "H")
+
+    def test_init_with_float_custom_id(self):
+        b = Base(8.5)
+        self.assertEqual(b.id, 8.5)
+
+# -----------------------------------------------------------------#
+
+class Test_Rectangle(unittest.TestCase):
+
+    def test_init(self):
+        r = Rectangle(2, 3)
+        self.assertEqual(r.id, 10)
+        self.assertEqual(r.width, 2)
+        self.assertEqual(r.height, 3)
+
+    def test_area(self):
+        r = Rectangle(2, 3)
+        self.assertEqual(r.area(), 6)
+
+    def test_Rectangle_width_neg(self):
+        with self.assertRaises(ValueError) as context:
+            Rectangle(-20, 7, 2, 8)
+        self.assertEqual(str(context.exception), "width must be > 0")
+
+    def test_Rectangle_width_zero(self):
+        with self.assertRaises(ValueError) as context:
+            Rectangle(0, 7, 2, 8)    
+        self.assertEqual(str(context.exception), "width must be > 0")
+
+    def test_Rectangle_height_neg(self):
+        with self.assertRaises(ValueError) as context:
+            Rectangle(10, 0, 2, 8)
+        self.assertEqual(str(context.exception), "height must be > 0")
+
+    def test_Rectangle_height_zero(self):
+        with self.assertRaises(ValueError) as context:
+            Rectangle(10, -7, 2, 8)
+        self.assertEqual(str(context.exception), "height must be > 0")
+
+    def test_Rectangle_x_zero(self):
+        with self.assertRaises(ValueError) as context:
+            Rectangle(10, 7, -5, 8)
+        self.assertEqual(str(context.exception), "x must be >= 0")
+
+    def test_Rectangle_y_zero(self):
+        with self.assertRaises(ValueError) as context:
+            Rectangle(10, 7, 0, -8)
+        self.assertEqual(str(context.exception), "y must be >= 0")
+
+# -----------------------------------------------------------------#
+
+class Test_Square(unittest.TestCase):
+
+    def test_init(self):
+        s = Square(4)
+        self.assertEqual(s.id, 11)
+        self.assertEqual(s.size, 4)
+
+    def test_size_property(self):
+        s = Square(4)
+        self.assertEqual(s.size, 4)
+        s.size = 5
+        self.assertEqual(s.size, 5)
+
+    def test_update(self):
+        s = Square(4)
+        s.update(6, size=7, x=1, y=2)
+        self.assertEqual(s.id, 6)
+        self.assertEqual(s.size, 4)
+        self.assertEqual(s.x, 0)
+        self.assertEqual(s.y, 0)
+
+    def test_Square_size_neg(self):
+        with self.assertRaises(ValueError) as context:
+            Square(10, 10, 2, -8)
+        self.assertEqual(str(context.exception), "y must be >= 0")
+
+    def test_Square_size_zero(self):
+        with self.assertRaises(ValueError) as context:
+            Square(10, -7, 2, 8)
+        self.assertEqual(str(context.exception), "x must be >= 0")
+
+# -----------------------------------------------------------------#
+
+class Test_JSON(unittest.TestCase):
+    def test_to_json_string(self):
+        b = Base(1)
+        expected_result = '[{"id": 1}]'
+        self.assertEqual(Base.to_json_string([{'id': b.id}]), expected_result)
+    
+    def test_to_json_string_rect(self):
+        r = Rectangle(10, 7, 2, 8)
+        objects = [r.to_dictionary()]
+        json_string = Base.to_json_string(objects)
+        expected_result = '[{"y": 8, "width": 10, "x": 2, "id": 2, "height": 7}]'
+        self.assertEqual(json_string, expected_result)
+
+if __name__ == '__main__':
+    unittest.main()
