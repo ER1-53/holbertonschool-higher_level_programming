0f71609256a7a834361c141cc4ff0829631f035d
ER1-53 <cremey.erwan@gmail.com>
2023-10-25 15:14:30 +0200

    test 1.3

--- python-almost_a_circle/tests/test.py
+++ python-almost_a_circle/tests/test.py
@@ -8,9 +8,6 @@ from models.square import Square
 
 
 class Test_Base(unittest.TestCase):
-    """Function to find and return the max integer in a list of integers
-        If the list is empty, the function returns None
-    """
     def test_base_initialization_id(self):
         b1 = Base()
         self.assertEqual(b1.id, 1)
