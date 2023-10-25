a7cc10f540309312a369f80cf52848a4c9c86aef
ER1-53 <cremey.erwan@gmail.com>
2023-10-25 15:46:17 +0200

    test bis ER v.4

--- python-almost_a_circle/tests/test.py
+++ python-almost_a_circle/tests/test.py
@@ -1,87 +1,67 @@
 #!/usr/bin/python3
-"""Module to test the condition of max integer in a list
+"""Unittest for class Base
 """
 import unittest
 from models.base import Base
 from models.rectangle import Rectangle
 from models.square import Square
 
+class TestBase(unittest.TestCase):
+    def test_base_initialization_id(self):
+        b1 = Base()
+        self.assertEqual(b1.id, 1)
+        b2 = Base()
+        self.assertEqual(b2.id, 2)
+        b3 = Base()
+        self.assertEqual(b3.id, 3)
+        b4 = Base(12)
+        self.assertEqual(b4.id, 12)
+        b5 = Base()
+        self.assertEqual(b5.id, 4)
 
-class Test(unittest.TestCase):
-    """Function to find and return the max integer in a list of integers
-        If the list is empty, the function returns None
-    """
-    
-    def test_init(self):
-        b = Base()
-        self.assertEqual(b.id, 1)
-        b = Base(100)
-        self.assertEqual(b.id, 100)
-        b = Base(-5)
-        self.assertEqual(b.id, -5)
-        b = Base("holbies")
-        self.assertEqual(b.id, "holbies")
-        b = Base("H")
-        self.assertEqual(b.id, "H")
-        b = Base(8.5)
-        self.assertEqual(b.id, 8.5)
-    
+    def test_rectangle_initialization_id(self):
+        with self.assertRaises(TypeError):
+            r1 = Rectangle()
+        with self.assertRaises(TypeError):
+            r2 = Rectangle(1)
+        r3 = Rectangle(2, 5)
+        self.assertEqual(r3.id, 5)
+        self.assertEqual(r3.width, 2)
+        self.assertEqual(r3.height, 5)
+        self.assertEqual(r3.x, 0)
+        self.assertEqual(r3.y, 0)
 
-    def test_init(self):
-        r = Rectangle(2, 3)
-        self.assertEqual(r.id, 10)
-        self.assertEqual(r.width, 2)
-        self.assertEqual(r.height, 3)
-        r = Rectangle(2, 3)
-        self.assertEqual(r.area(), 6)
-        with self.assertRaises(ValueError) as context:
-            Rectangle(-20, 7, 2, 8)
-        self.assertEqual(str(context.exception), "width must be > 0")
-        with self.assertRaises(ValueError) as context:
-            Rectangle(0, 7, 2, 8)    
-        self.assertEqual(str(context.exception), "width must be > 0")
-        with self.assertRaises(ValueError) as context:
-            Rectangle(10, 0, 2, 8)
-        self.assertEqual(str(context.exception), "height must be > 0")
-        with self.assertRaises(ValueError) as context:
-            Rectangle(10, -7, 2, 8)
-        self.assertEqual(str(context.exception), "height must be > 0")
-        with self.assertRaises(ValueError) as context:
-            Rectangle(10, 7, -5, 8)
-        self.assertEqual(str(context.exception), "x must be >= 0")
-        with self.assertRaises(ValueError) as context:
-            Rectangle(10, 7, 0, -8)
-        self.assertEqual(str(context.exception), "y must be >= 0")
+        r4 = Rectangle(3,6,1)
+        self.assertEqual(r4.id, 6)
+        self.assertEqual(r4.width, 3)
+        self.assertEqual(r4.height, 6)
+        self.assertEqual(r4.x, 1)
+        self.assertEqual(r4.y, 0)
+        r5 = Rectangle(1 ,2, 3, 4, 20)
+        self.assertEqual(r5.id, 20)
+        self.assertEqual(r5.width, 1)
+        self.assertEqual(r5.height, 2)
+        self.assertEqual(r5.x, 3)
+        self.assertEqual(r5.y, 4)
 
+        with self.assertRaises(ValueError):
+            r5 = Rectangle(0, 0)
+        with self.assertRaises(ValueError):
+            r5 = Rectangle(5, -5)
+        with self.assertRaises(TypeError):
+            r5 = Rectangle(5, "-5")
+        with self.assertRaises(TypeError):
+            r5 = Rectangle(5, [-5])
+        with self.assertRaises(TypeError):
+            r5 = Rectangle(5, {-5})
 
-    def test_init(self):
-        s = Square(4)
-        self.assertEqual(s.id, 1)
-        self.assertEqual(s.size, 4)
-        s = Square(4)
-        self.assertEqual(s.size, 4)
-        s.size = 5
-        self.assertEqual(s.size, 5)
-        s = Square(4)
-        s.update(6, size=7, x=1, y=2)
-        self.assertEqual(s.id, 6)
-        self.assertEqual(s.size, 4)
-        self.assertEqual(s.x, 0)
-        self.assertEqual(s.y, 0)
-        with self.assertRaises(ValueError) as context:
-            Square(10, -7)
-        self.assertEqual(str(context.exception), "x must be >= 0")
+    def test_square_initialization_id(self):
+        with self.assertRaises(TypeError):
+            s1 = Square()
+        s1 = Square(3)
+        self.assertEqual(s1.id, 12)
 
-
-    def test_to_json_string(self):
-        b = Base(1)
-        expected_result = '[{"id": 1}]'
-        self.assertEqual(Base.to_json_string([{'id': b.id}]), expected_result)
-        r = Rectangle(10, 7, 2, 8)
-        objects = [r.to_dictionary()]
-        json_string = Base.to_json_string(objects)
-        expected_result = '[{"y": 8, "width": 10, "x": 2, "id": 5, "height": 7}]'
-        self.assertEqual(json_string, expected_result)
 
 if __name__ == '__main__':
     unittest.main()
+    
