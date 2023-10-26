1075efb3815f7fc743332774342d3047616fce1c
ER1-53 <cremey.erwan@gmail.com>
2023-10-26 21:54:39 +0200

    test of test 1.1

--- python-almost_a_circle/tests/test_list.py
+++ python-almost_a_circle/tests/test_list.py
@@ -22,5 +22,12 @@ class Test_General(unittest.TestCase):
         b4 = Base(12)
         b5 = Base()
 
+        # Vérifiez les valeurs des attributs 'id'
+        self.assertEqual(b1.id, 1)
+        self.assertEqual(b2.id, 2)
+        self.assertEqual(b3.id, 3)
+        self.assertEqual(b4.id, 12)
+        self.assertEqual(b5.id, 4)
+
 if __name__ == '__main__':
     unittest.main()
