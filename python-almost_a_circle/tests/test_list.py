0de5df6c76b78ca95b86e4572ffa57edf0a17fdd
ER1-53 <cremey.erwan@gmail.com>
2023-10-26 22:03:40 +0200

    test of test v1.4

--- python-almost_a_circle/tests/test_list.py
+++ python-almost_a_circle/tests/test_list.py
@@ -14,28 +14,15 @@ class Test_General(unittest.TestCase):
     """Function to find and return the max integer in a list of integers
         If the list is empty, the function returns None
     """
-    def test_rectangle_exceptions(self):
-        # Test de l'exception de type pour la hauteur
-        with self.assertRaises(TypeError) as cm:
-            Rectangle(10, True)
-        self.assertEqual(str(cm.exception), "height must be an integer")
+    def test_rectangle_area(self):
+        r1 = Rectangle(3, 2)
+        self.assertEqual(r1.area(), 6)
 
-        # Test de l'exception de valeur pour la largeur
-        with self.assertRaises(ValueError) as cm:
-            r = Rectangle(10, 2)
-            r.width = -10
-        self.assertEqual(str(cm.exception), "width must be > 0")
+        r2 = Rectangle(2, 10)
+        self.assertEqual(r2.area(), 20)
 
-        # Test de l'exception de type pour la position x
-        with self.assertRaises(TypeError) as cm:
-            r = Rectangle(10, 2, True)
-            r.x = {}
-        self.assertEqual(str(cm.exception), "x must be an integer")
-
-        # Test de l'exception de valeur pour la position y
-        with self.assertRaises(ValueError) as cm:
-            Rectangle(10, 2, 3, -1)
-        self.assertEqual(str(cm.exception), "y must be >= 0")
+        r3 = Rectangle(8, 7, 0, 0, 12)
+        self.assertEqual(r3.area(), 56)
 
 if __name__ == '__main__':
     unittest.main()
