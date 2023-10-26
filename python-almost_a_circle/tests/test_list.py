41e7b0fb609361302901b02a84a5856b37dce960
ER1-53 <cremey.erwan@gmail.com>
2023-10-26 22:06:07 +0200

    test of test 1.5

--- python-almost_a_circle/tests/test_list.py
+++ python-almost_a_circle/tests/test_list.py
@@ -14,15 +14,23 @@ class Test_General(unittest.TestCase):
     """Function to find and return the max integer in a list of integers
         If the list is empty, the function returns None
     """
-    def test_rectangle_area(self):
-        r1 = Rectangle(3, 2)
-        self.assertEqual(r1.area(), 6)
+    def test_rectangle_display(self):
+        r1 = Rectangle(4, 6)
+        expected_output = "####\n####\n####\n####\n####\n####\n"
+        
+        with unittest.mock.patch('sys.stdout', new_callable=StringIO) as mock_stdout:
+            r1.display()
+            self.assertEqual(mock_stdout.getvalue(), expected_output)
 
-        r2 = Rectangle(2, 10)
-        self.assertEqual(r2.area(), 20)
+        # Réinitialisez le flux de sortie standard
+        mock_stdout = StringIO()
 
-        r3 = Rectangle(8, 7, 0, 0, 12)
-        self.assertEqual(r3.area(), 56)
+        r2 = Rectangle(2, 2)
+        expected_output = "##\n##\n"
+        
+        with unittest.mock.patch('sys.stdout', new_callable=StringIO) as mock_stdout:
+            r2.display()
+            self.assertEqual(mock_stdout.getvalue(), expected_output)
 
 if __name__ == '__main__':
     unittest.main()
