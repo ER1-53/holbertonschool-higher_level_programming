1ec0fa13360e92c0198ddb98c4f4c8714da24dcf
ER1-53 <cremey.erwan@gmail.com>
2023-11-20 15:30:32 +0100

    try without auto_increment

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,4 +1,5 @@
--- 9-full_creation.sql
+-- 9-full_creation.sql CREATE TABLE second_table(id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(256), score INT);
+--INSERT INTO second_table (name, score) VALUES ('John', 10), ('Alex', 3), ('Bob', 14), ('George', 8);
 
-CREATE TABLE second_table(id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(256), score INT);
-INSERT INTO second_table (name, score) VALUES ('John', 10), ('Alex', 3), ('Bob', 14), ('George', 8);
+CREATE TABLE second_table(id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, 'George', 8);
