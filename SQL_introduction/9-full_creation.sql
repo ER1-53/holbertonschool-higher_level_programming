34dec9e6a198a0868e5da7ee5d4959e80660ee78
ER1-53 <cremey.erwan@gmail.com>
2023-11-20 15:28:24 +0100

    finish by ;

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,4 +1,4 @@
 -- 9-full_creation.sql
 
 CREATE TABLE second_table(id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(256), score INT);
-INSERT INTO second_table (name, score) VALUES ('John', 10), ('Alex', 3), ('Bob', 14), ('George', 8)
+INSERT INTO second_table (name, score) VALUES ('John', 10), ('Alex', 3), ('Bob', 14), ('George', 8);
