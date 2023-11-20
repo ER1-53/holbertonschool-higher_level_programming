46d548472dd0041a2a6c5b214dffcc4dd6091f25
ER1-53 <cremey.erwan@gmail.com>
2023-11-20 15:37:24 +0100

    oder desc

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,3 +1,3 @@
 -- 15-groups.sql
 
-SELECT  score, COUNT(score) AS number FROM second_table GROUP BY score;
+SELECT  score, COUNT(score) AS number FROM second_table GROUP BY score ORDER BY score DESC;
