a83dfed7f962d6f8adc18cf51957f32d69f64f17
ER1-53 <cremey.erwan@gmail.com>
2023-11-26 19:50:18 +0100

    task 2 and 4

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -3,28 +3,24 @@ import MySQLdb
 import sys
 
 
-if len(sys.argv) != 5:
-    print("Usage: {} <username> <password>\
-          <database> <state_name>".format(sys.argv[0]))
+if len(sys.argv) != 4:
+    print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
     sys.exit(1)
 
 MY_USER = sys.argv[1]
 MY_PASSWORD = sys.argv[2]
 MY_DB = sys.argv[3]
 MY_HOST = 'localhost'
-STATE_NAME_SEARCHED = sys.argv[4]
 
 try:
     # Connect to MySQL server
-    db = MySQLdb.connect(host=MY_HOST, port=3306, user=MY_USER,
-                         passwd='', db=MY_DB)
+    db = MySQLdb.connect(host=MY_HOST, port=3306, user=MY_USER, passwd='', db=MY_DB)
 
     # create an instance of exec querys
     cursor = db.cursor()
 
     # Execute the SELECT query and protect to sql injection
-    cursor.execute("SELECT * FROM states WHERE\
-                   name LIKE %s ORDER BY id ASC", (STATE_NAME_SEARCHED,))
+    cursor.execute("SELECT * FROM cities ORDER BY id ASC")
 
     # Fetch all the rows
     rows = cursor.fetchall()
