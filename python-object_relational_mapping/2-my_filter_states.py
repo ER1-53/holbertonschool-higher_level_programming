b81e26b7ff1f20128d1c8515e6a1af386307e41c
ER1-53 <cremey.erwan@gmail.com>
2023-11-27 18:15:29 +0100

    if __name__ == "__main__":

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -2,40 +2,40 @@
 import MySQLdb
 import sys
 
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        print("Usage: {} <username> <password>\
+            <database> <state_name>".format(sys.argv[0]))
+        sys.exit(1)
 
-if len(sys.argv) != 5:
-    print("Usage: {} <username> <password>\
-          <database> <state_name>".format(sys.argv[0]))
-    sys.exit(1)
+    MY_USER = sys.argv[1]
+    MY_PASSWORD = sys.argv[2]
+    MY_DB = sys.argv[3]
+    MY_HOST = 'localhost'
+    STATE_NAME_SEARCHED = sys.argv[4]
 
-MY_USER = sys.argv[1]
-MY_PASSWORD = sys.argv[2]
-MY_DB = sys.argv[3]
-MY_HOST = 'localhost'
-STATE_NAME_SEARCHED = sys.argv[4]
+    try:
+        # Connect to MySQL server
+        dbase = MySQLdb.connect(host=MY_HOST, port=3306, user=MY_USER, passwd='', db=MY_DB)
 
-try:
-    # Connect to MySQL server
-    dbase = MySQLdb.connect(host=MY_HOST, port=3306, user=MY_USER, passwd='', db=MY_DB)
+        # create an instance of exec querys
+        cursor = dbase.cursor()
 
-    # create an instance of exec querys
-    cursor = dbase.cursor()
+        # Execute the SELECT query and protect to sql injection
+        cursor.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC", (STATE_NAME_SEARCHED,))
 
-    # Execute the SELECT query and protect to sql injection
-    cursor.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC", (STATE_NAME_SEARCHED,))
+        # Fetch all the rows
+        rows = cursor.fetchall()
 
-    # Fetch all the rows
-    rows = cursor.fetchall()
+        # Display the results
+        for row in rows:
+            print(row)
 
-    # Display the results
-    for row in rows:
-        print(row)
+    except MySQLdb.Error as e:
+        print("MySQL Error: {}".format(e))
 
-except MySQLdb.Error as e:
-    print("MySQL Error: {}".format(e))
-
-finally:
-    # Close the database connection
-    if dbase:
-        cursor.close()
-        dbase.close()
+    finally:
+        # Close the database connection
+        if dbase:
+            cursor.close()
+            dbase.close()
