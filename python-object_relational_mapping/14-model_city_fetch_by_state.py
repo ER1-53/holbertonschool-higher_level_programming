edd205fc8f6a4d6a0b74f649dd6fda0ef2de9829
ER1-53 <cremey.erwan@gmail.com>
2023-11-27 19:13:20 +0100

    task 8 to 14

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
 """Start link class to table in database"""
-import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sys import argv
 from model_state import Base, State
 from model_city import City
-
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
 
 if __name__ == "__main__":
-    arg = (sys.argv[1], sys.argv[2], sys.argv[3])
-    engine = create_engine('mysql+mysqldb:\
-                           //{}:{}@localhost/{}\
-                           '.format(arg), pool_pre_ping=True)
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]),
+        pool_pre_ping=True
+        )
     Base.metadata.create_all(engine)
 
 Session = sessionmaker(bind=engine)
--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 """Start link class to table in database"""
-import sys
+from sys import argv
 from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
-
 if __name__ == "__main__":
-    arg = (sys.argv[1], sys.argv[2], sys.argv[3])
-    engine = create_engine('mysql+mysqldb:\
-                           //{}:{}@localhost/{}\
-                           '.format(arg), pool_pre_ping=True)
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]),
+        pool_pre_ping=True
+        )
     Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
