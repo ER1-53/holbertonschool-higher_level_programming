acf59c03885b3998aefdc975bd8a030b4649e4fc
ER1-53 <cremey.erwan@gmail.com>
2023-11-27 18:24:09 +0100

    simplification task 2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,37 +1,16 @@
 #!/usr/bin/python3
+"""Module listing all Arizona name from the database"""
 import MySQLdb
-import sys
+from sys import argv
 
-
-
-MY_USER = sys.argv[1]
-MY_PASSWORD = sys.argv[2]
-MY_DB = sys.argv[3]
-MY_HOST = 'localhost'
-STATE_NAME_SEARCHED = sys.argv[4]
-
-try:
-    # Connect to MySQL server
-    dbase = MySQLdb.connect(host='localhost', port=3306, user=MY_USER, passwd='', db=MY_DB)
-
-    # create an instance of exec querys
-    cursor = dbase.cursor()
-
-    # Execute the SELECT query and protect to sql injection
-    cursor.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC", (STATE_NAME_SEARCHED,))
-
-    # Fetch all the rows
-    rows = cursor.fetchall()
-
-    # Display the results
-    for row in rows:
+db = MySQLdb.connect("localhost", argv[1], argv[2], argv[3])
+cursor = db.cursor()
+cursor.execute(
+    """SELECT * FROM states WHERE name = '{}' ORDER BY id ASC"""
+    .format(argv[4]))
+rows = cursor.fetchall()
+for row in rows:
+    if row[1] == argv[4]:
         print(row)
-
-except MySQLdb.Error as e:
-    print("MySQL Error: {}".format(e))
-
-finally:
-    # Close the database connection
-    if dbase:
-        cursor.close()
-        dbase.close()
+cursor.close()
+db.close()
